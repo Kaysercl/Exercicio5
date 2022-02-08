e2d044fb03e16c340676f9bc4ba472834a4ef122
Kayser <Kayser@DESKTOP-JD3R4EO>
2022-02-08 15:00:19 -0300

    Add project files.

---
+++ Exercicio5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exercicio5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o valor de A");
+            int A = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor de B");
+            int B = int.Parse(Console.ReadLine());
+            int R = A + B;
+            int Resultado = A * B;
+
+            if (A == B)
+            {
+                Console.WriteLine("O resultado de A+B é igual a " + R);
+            }
+            else
+            {
+                Console.WriteLine("O resultado de A*B é igual a " + Resultado);
+            }
+            
+            
+
+        }
+    }
+}
